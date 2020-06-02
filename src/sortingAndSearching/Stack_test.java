3fd394b4eb3788ad7248c478e3420ede46a89974
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2020-06-03 01:23:19 +0900

    Create Stack_test.java

---
+++ src/sortingAndSearching/Stack_test.java
@@ -0,0 +1,13 @@
+package sortingAndSearching;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Stack_test {
+	private void insert(int[] arr) {
+		LinkedList<Integer> cup = new LinkedList<Integer>();
+		for (Integer element : arr) {
+			cup.add(arr[element]);
+		}
+	}
+}
